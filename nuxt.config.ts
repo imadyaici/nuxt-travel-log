eb7113bcd4fc1a92595377d633adc5b0962ca79f
Imad Yaici <yaici.imad@gmail.com>
2025-08-01 23:07:18 +0100

    ✨ typesafe env setup (#50)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 
+import "./lib/env"; // Ensure environment variables are loaded before Nuxt config
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
