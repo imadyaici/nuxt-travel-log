3901a2945416cab2548ec8f43cb3ee7b3ff563c0
Imad Yaici <yaici.imad@gmail.com>
2025-08-02 14:58:04 +0100

    setup db

--- lib/env.ts
+++ lib/env.ts
@@ -4,8 +4,8 @@ import tryParseEnv from "./try-parse-env";
 
 const EnvSchema = z.object({
   NODE_ENV: z.string(),
-  // TURSO_DATABASE_URL: z.string(),
-  // TURSO_AUTH_TOKEN: z.string(),
+  TURSO_DATABASE_URL: z.string(),
+  TURSO_AUTH_TOKEN: z.string(),
   // BETTER_AUTH_SECRET: z.string(),
   // BETTER_AUTH_URL: z.string(),
   // AUTH_GITHUB_CLIENT_ID: z.string(),
