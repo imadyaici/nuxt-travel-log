5af6b5d86f5cf298af7b10928fc12233c1de05d4
Imad Yaici <yaici.imad@gmail.com>
2025-08-02 15:48:54 +0100

    ⚡️setup auth

--- lib/db/schema/location-log.ts
+++ lib/db/schema/location-log.ts
@@ -1,14 +1,13 @@
-import { relations } from "drizzle-orm";
+// import { relations } from "drizzle-orm";
 import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
-import { createInsertSchema } from "drizzle-zod";
-import { z } from "zod";
+// import { createInsertSchema } from "drizzle-zod";
+// import { z } from "zod";
 
-import type { SelectLocationLogImage } from "./location-log-image";
-
-import { DateSchema, DescriptionSchema, LatSchema, LongSchema, NameSchema } from "../../zod-schemas";
+// import type { SelectLocationLogImage } from "./location-log-image";
+// import { DateSchema, DescriptionSchema, LatSchema, LongSchema, NameSchema } from "../../zod-schemas";
 import { user } from "./auth";
 import { location } from "./location";
-import { locationLogImage } from "./location-log-image";
+// import { locationLogImage } from "./location-log-image";
 
 export const locationLog = sqliteTable("locationLog", {
   id: int().primaryKey({ autoIncrement: true }),
@@ -24,44 +23,44 @@ export const locationLog = sqliteTable("locationLog", {
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
 });
 
-export const locationLogRelations = relations(locationLog, ({ one, many }) => ({
-  location: one(location, {
-    fields: [locationLog.locationId],
-    references: [location.id],
-  }),
-  images: many(locationLogImage),
-}));
+// export const locationLogRelations = relations(locationLog, ({ one, many }) => ({
+//   location: one(location, {
+//     fields: [locationLog.locationId],
+//     references: [location.id],
+//   }),
+//   images: many(locationLogImage),
+// }));
 
-export const InsertLocationLog = createInsertSchema(locationLog, {
-  name: NameSchema,
-  description: DescriptionSchema,
-  lat: LatSchema,
-  long: LongSchema,
-  startedAt: DateSchema,
-  endedAt: DateSchema,
-}).omit({
-  id: true,
-  userId: true,
-  locationId: true,
-  createdAt: true,
-  updatedAt: true,
-}).superRefine((values, context) => {
-  if (values.startedAt > values.endedAt || values.endedAt < values.startedAt) {
-    context.addIssue({
-      code: z.ZodIssueCode.custom,
-      message: "Start Date must be before End Date",
-      path: ["startedAt"],
-    });
-    context.addIssue({
-      code: z.ZodIssueCode.custom,
-      message: "End Date must be after Start Date",
-      path: ["endedAt"],
-    });
-  }
-});
+// export const InsertLocationLog = createInsertSchema(locationLog, {
+//   name: NameSchema,
+//   description: DescriptionSchema,
+//   lat: LatSchema,
+//   long: LongSchema,
+//   startedAt: DateSchema,
+//   endedAt: DateSchema,
+// }).omit({
+//   id: true,
+//   userId: true,
+//   locationId: true,
+//   createdAt: true,
+//   updatedAt: true,
+// }).superRefine((values, context) => {
+//   if (values.startedAt > values.endedAt || values.endedAt < values.startedAt) {
+//     context.addIssue({
+//       code: z.ZodIssueCode.custom,
+//       message: "Start Date must be before End Date",
+//       path: ["startedAt"],
+//     });
+//     context.addIssue({
+//       code: z.ZodIssueCode.custom,
+//       message: "End Date must be after Start Date",
+//       path: ["endedAt"],
+//     });
+//   }
+// });
 
-export type InsertLocationLog = z.infer<typeof InsertLocationLog>;
-export type SelectLocationLog = typeof locationLog.$inferSelect;
-export type SelectLocationLogWithImages = SelectLocationLog & {
-  images: SelectLocationLogImage[];
-};
+// export type InsertLocationLog = z.infer<typeof InsertLocationLog>;
+// export type SelectLocationLog = typeof locationLog.$inferSelect;
+// export type SelectLocationLogWithImages = SelectLocationLog & {
+//   images: SelectLocationLogImage[];
+// };
