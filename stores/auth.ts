dfd5c010886ca1c05ea5eb9d8b6a69f9697c59b5
Imad Yaici <yaici.imad@gmail.com>
2025-08-04 16:58:46 +0100

    :sparkles: protected routes and ssr auth session (#54)

--- stores/auth.ts
+++ stores/auth.ts
@@ -3,13 +3,12 @@ import { createAuthClient } from "better-auth/vue";
 const authClient = createAuthClient();
 
 export const useAuthStore = defineStore("authStore", () => {
-  // const session = ref<Awaited<ReturnType<typeof authClient.useSession>> | null>(null);
-  const session = authClient.useSession();
+  const session = ref<Awaited<ReturnType<typeof authClient.useSession>> | null>(null);
 
-  // async function init() {
-  //   const data = await authClient.useSession(useFetch);
-  //   session.value = data;
-  // }
+  async function init() {
+    const data = await authClient.useSession(useFetch);
+    session.value = data;
+  }
 
   const user = computed(() => session.value?.data?.user);
   const loading = computed(() => session.value?.isPending);
@@ -41,7 +40,7 @@ export const useAuthStore = defineStore("authStore", () => {
   }
 
   return {
-    // init,
+    init,
     loading,
     signIn,
     signOut,
