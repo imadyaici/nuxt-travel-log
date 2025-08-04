63f8547527632ef1b7507c19f97b6a3eec053ba5
Imad Yaici <yaici.imad@gmail.com>
2025-08-04 16:57:50 +0100

    :sparkles: protected routes and ssr auth session

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,30 +1,30 @@
-// import type { User } from "better-auth";
+import type { User } from "better-auth";
 
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-// import { createAuthMiddleware } from "better-auth/plugins";
+import { createAuthMiddleware } from "better-auth/plugins";
 
 import db from "./db";
 import env from "./env";
 
-// export type UserWithId = Omit<User, "id"> & {
-//   id: number;
-// };
+export type UserWithId = Omit<User, "id"> & {
+  id: number;
+};
 
 export const auth = betterAuth({
-  // hooks: {
-  //   after: createAuthMiddleware(async (ctx) => {
-  //     if (ctx.path === "/get-session") {
-  //       if (!ctx.context.session) {
-  //         return ctx.json({
-  //           session: null,
-  //           user: null,
-  //         });
-  //       }
-  //       return ctx.json(ctx.context.session);
-  //     }
-  //   }),
-  // },
+  hooks: {
+    after: createAuthMiddleware(async (ctx) => {
+      if (ctx.path === "/get-session") {
+        if (!ctx.context.session) {
+          return ctx.json({
+            session: null,
+            user: null,
+          });
+        }
+        return ctx.json(ctx.context.session);
+      }
+    }),
+  },
   database: drizzleAdapter(db, {
     provider: "sqlite",
   }),
